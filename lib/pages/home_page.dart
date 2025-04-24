958a0fae32a8e54b80b0dc1818888fc8ecf17f86
Xxvedant7xX <agrawalvedant2006@gmail.com>
2025-04-24 19:53:03 +0530

    completed project

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -65,6 +67,119 @@ class HomePage extends StatelessWidget {
             label: '',
           ),
         ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20),
+                child: Text('Discover  The  Ultimate Project  Platform  for Managemenet',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey,
+                        hintText: 'Search Project',
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: Colors.cyan[100],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.search,
+                        color: Colors.white,
+                        size: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+                ),
+              ),
+              
+              SizedBox(height: 40),
+          
+              Column(
+                children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan[100],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('   Project Tools   ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                   ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                 Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan[100],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('     Resources     ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                   ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                 Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan[100],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('       Support       ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                   ),
+                  ),
+                ),
+              ],
+              ),
+          ],
+          ),
+        ),
       ),
     );
   }
